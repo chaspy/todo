41d7dd5143f78c0864e0c18442d7e30192ad4b0c
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 11:42:25 +0900

    Use task resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root to: 'task#index'
-  get 'task/show'
-  get 'task/index'
-  get 'task/update'
-  get 'task/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :task
 end
