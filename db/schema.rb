2b67714c8395f3fcf92df6351eddf34f7f67a63d
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 11:24:19 +0900

    rails g model Tag name:string rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_021923) do
+ActiveRecord::Schema.define(version: 2018_11_10_022348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
