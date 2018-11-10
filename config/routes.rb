d62b7e2f46e700a6b2abdbf68af2a3646928c9cb
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 18:55:41 +0900

    Google logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
-
   devise_for :users, controllers: {
       omniauth_callbacks: "users/omniauth_callbacks"
   }
+  devise_scope :user do
+    delete '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
