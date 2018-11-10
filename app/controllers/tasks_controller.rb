eead60f1fc5ac524a2c921fffbbd925426b1dbe6
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 13:00:55 +0900

    Rename tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,13 @@
-class TaskController < ApplicationController
+class TasksController < ApplicationController
 
   def new
     @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url
   end
 
   def show
@@ -14,5 +20,11 @@ class TaskController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'task#index'
-  resources :task
+  root to: 'tasks#index'
+  resources :tasks
 end
