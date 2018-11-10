504de4c584c0f0617673e42bda7f90d304a8045f
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-11 03:06:53 +0900

    Add user_id to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   has_many :tasks
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :trackable, :omniauthable, omniauth_providers: %i(google)
   has_many :tasks
+  has_many :tags
 
   protected
   def self.find_for_google(auth)
