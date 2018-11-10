4190a6aa579a88bd243355160902ab9d456bce27
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 11:25:40 +0900

    rails g model Task name:string description:text user_id:int tag_id:int image_url:string

---
+++ db/migrate/20181110022527_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.int :user_id
+      t.int :tag_id
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
