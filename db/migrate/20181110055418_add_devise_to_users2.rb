689ca2daecd11055526895ad18ed7740a51bcdc2
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 18:26:52 +0900

    Add migrate

---
+++ db/migrate/20181110055418_add_devise_to_users2.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AddDeviseToUsers2 < ActiveRecord::Migration[5.2]
+  def self.up
+    change_table :users do |t|
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
+    end
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
