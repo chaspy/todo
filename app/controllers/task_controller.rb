9d75ad112b8d9f57fe1321235ba14a961995a2e0
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 11:37:44 +0900

    rails g controller task show index update destroy

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,13 @@
+class TaskController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'task/show'
+  get 'task/index'
+  get 'task/update'
+  get 'task/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
