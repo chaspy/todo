ac42502e46304d6f586a9bd9882cd776cb0a5b20
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 18:27:12 +0900

    Hello, Google Outh

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :trackable, :omniauthable, omniauth_providers: %i(google)
+
+  protected
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.create(email: auth.info.email,
+                         encrypted_password: Devise.friendly_token[0, 20])
+    end
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'tasks#index'
   resources :tasks
+
+  devise_for :users, controllers: {
+      omniauth_callbacks: "users/omniauth_callbacks"
+  }
 end
