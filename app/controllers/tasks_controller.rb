4c4c256c0285ff56e707eeb0c3727b6201e99f5c
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 14:17:06 +0900

    Tasks#edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,14 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url
   end
 
   def destroy
