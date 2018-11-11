5382881b83a8681be7a016dfd7dc060ebda2f759
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-11 12:47:05 +0900

    Add validation to model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :tasks
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 64 }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :tag
+
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :description, length: { maximum: 512 }
+  validates :expire_date, presence: true
 end
